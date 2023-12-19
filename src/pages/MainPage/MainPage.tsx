202071a02362bcef620a3a807943a68da1b15f1d
Aswin Asok <aswinasokofficial@gmail.com>
2023-12-19 21:09:04 +0530

    feat(sidebar):initial-sidebar-done

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,7 +1,24 @@
+import styles from './MainPage.module.css'
+
 const MainPage = () => {
-  return (
-    <div>MainPage</div>
-  )
+    return (
+        <div className={styles.mainContainer}>
+            <div className={styles.projectsSideBarContainer}>
+                <div className={styles.projectsSideBar}>
+                    <p className={styles.projectsSideBarHeading}>Projects</p>
+                    <div className={styles.projects}>
+                        <div className={styles.project}>AB</div>
+                        <div className={styles.project}>CD</div>
+                        <div className={styles.project}>EF</div>
+                        <div className={styles.project}>GH</div>
+                    </div>
+                    <div className={styles.addProjects}>
+                        <p className={styles.addProjectsPlus}>+</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default MainPage
