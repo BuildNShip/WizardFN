eb1bd9ce4a22fe130936e7643f8c84e0ce3e39a5
Aswin Asok <aswinasokofficial@gmail.com>
2023-12-20 16:44:03 +0530

    feat(collections):inital-div-done

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -5,6 +5,23 @@ const MainPage = () => {
     return (
         <div className={styles.mainContainer}>
             <Sidebar />
+
+            <div className={styles.collectionsContainer}>
+                <div className={styles.collectionsTopbar}>
+                    <div className={styles.collectionsTopbarUsername}>
+                        <div className={styles.collectionTopbarAvatar}>S</div>
+                        <div className={styles.collectionTopbarName}>Salman Faariz</div>
+                    </div>
+                </div>
+                <div className={styles.collectionsMenuContainer}>
+                    <div className={styles.searchBar}>
+                        <input className={styles.searchMenu} type="text" placeholder="Search files, teams or people" />
+                    </div>
+                    <div className={styles.collectionsMenu}>
+                        
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
