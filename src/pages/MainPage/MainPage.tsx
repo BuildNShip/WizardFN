186341a3ca6e064191606ef6d6afb5b061550601
Aswin Asok <aswinasokofficial@gmail.com>
2023-12-20 18:15:09 +0530

    feat(mainapp):top-bar-ui

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -2,12 +2,44 @@ import Collections from '../../components/Collections/Collections'
 import Sidebar from '../../components/Sidebar/Sidebar'
 import styles from './MainPage.module.css'
 
+import { LuUploadCloud } from "react-icons/lu";
+
 const MainPage = () => {
     return (
         <div className={styles.mainContainer}>
             <Sidebar />
 
             <Collections />
+
+            <div className={styles.mainAppContainer}>
+                <div className={styles.mainAppTopbar}>
+
+                    <div className={styles.topbarTabsContainer}>
+                        <div className={styles.topbarTabs}>
+                            <div className={styles.topbarTabActive}>
+                                <span className={styles.tabType}>
+                                    Drafts
+                                </span>
+                                Untitled
+                            </div>
+                            <div className={styles.topbarTab}>
+                                Another Tab
+                            </div>
+                        </div>
+                        <div className={styles.topbarAddTab}>
+                            +
+                        </div>
+                        <div className={styles.shareButton}>
+                            Share
+                        </div>
+                    </div>
+
+                    <div className={styles.topbarActions}>
+                        <button className={styles.saveToWorkspace}><LuUploadCloud className={styles.saveButton} /> Save My Workspace</button>
+                        <button className={styles.logoutButton}>Logout</button>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
